0dc62231248cec997172dae1295200e7abe3d57c
brokamal <kamalbc@keemail.me>
2025-11-13 10:23:45 +0700

    add nativewind

--- app/Home.jsx
+++ app/Home.jsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Header } from '../components/Header';
 import { TaskList } from '../components/TaskList'
 
+import "../global.css"
 
 
 export default function Home() {
